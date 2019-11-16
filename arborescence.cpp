eaab4d8da7719f3379057ae8fad890cb90677758
Abhishek Yadav <abhi.cs.0217@gmail.com>
2019-11-17 05:19:47 +0530

    Errors/bugs resolved

--- arborescence.cpp
+++ arborescence.cpp
@@ -7,8 +7,17 @@ void dfs(vector<vector<pair<int, int>> >& graph, int root, int* vis){
     cout << root+1 << " ";
     int i, j;
     for(i = 0; i < graph[root].size(); i++){
-        if(vis[graph[root][i].first] == false){
-            dfs(graph, graph[root][i].first, vis);
+        if(vis[graph[root][i].second] == false){
+            dfs(graph, graph[root][i].second, vis);
+        }
+    }
+}
+
+void print_graph(vector<vector<pair<int, int>> >& graph){
+    int n = graph.size(), i, j;
+    for(i = 0; i < n; i++){
+        for(j = 0; j < graph[i].size(); j++){
+            cout << i+1 << " " << graph[i][j].second+1 << " " << graph[i][j].first << "\n";
         }
     }
 }
@@ -38,38 +47,45 @@ int main(){
 			}
 		}
 
-        cout << "\n";
+        cout << "\nDFS done\n";
         
         for(i = 0; i < n; i++){
-            if(graph[i].begin() == graph[i].end())
-                continue;
             for(j = 0; j < graph[i].size(); j++){
                 cout << i+1 << " " << graph[i][j].second+1 << " " << graph[i][j].first << "\n";
                 entering_graph[graph[i][j].second].push_back({graph[i][j].first, i});
                 temp_graph[graph[i][j].second].push_back({graph[i][j].first, i});
             }
         }
+        
+        cout << "\nEntering graph done\n";
+        print_graph(temp_graph);
         cout << "\n";
         
         int flag = false;
         for(i = 0; i < n; i++){
-            if(graph[i].begin() == graph[i].end())
-                continue;
-            sort(temp_graph[i].begin(), temp_graph[i].end());
-            if(temp_graph[i][0].second == root){
-            	flag = true;
+            if(temp_graph[i].size() > 0){
+                sort(temp_graph[i].begin(), temp_graph[i].end());
+                if(temp_graph[i][0].second == root){
+                    flag = true;
+                }
             }
         }
+
+        cout << "flag = " << flag << "\n";
         
         if(flag == false){
-        	vector<pair<int, int>> v = graph[root];
-        	if(v.size() == 0){
+        	if(graph[root].size() == 0){
         		cout << "No edge comming out of root node!\n" ;
         	}
         	else{
+            	vector<pair<int, int>> v = graph[root];
         		sort(v.begin(), v.end());
-        		temp_graph[v[0].second][0] = v[0];
+        		temp_graph[v[0].second][0].first = v[0].first;
+        		temp_graph[v[0].second][0].second = root;
         	}
+             cout << "\nroot node thing done\n";
+            print_graph(temp_graph);
+            cout << "\n";
         }
 
     }
